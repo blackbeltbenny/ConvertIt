838f088579e5032b2fc0a9c4e5e342f3e8ca57f4
benmeisenzahl <benny.meisenzahl@gmail.com>
2018-10-04 11:24:03 -0400

    6.4, Switch Case statements!

--- ConvertIt/ViewController.swift
+++ ConvertIt/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     var formulaArray = ["miles to kilometers", "kilometers to miles", "feet to meters", "yards to meters", "meters to feet", "meters to yards"]
     var fromUnits = ""
     var toUnits = ""
+    var conversionString = ""
     
     @IBOutlet weak var userInput: UITextField!
     @IBOutlet weak var fromUnitsLabel: UILabel!
@@ -23,6 +24,36 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         formulaPicker.delegate = self
         formulaPicker.dataSource = self
+    }
+    
+    func calculateConversion(){
+        
+        var outputValue = 0.0
+        
+        if let inputValue = Double(userInput.text!){
+        
+            switch conversionString {
+            case "miles to kilometers":
+                outputValue = inputValue / 0.62137
+            case "kilometers to miles":
+                outputValue = inputValue * 0.62137
+            case "feet to meters":
+                outputValue = inputValue / 3.2808
+            case "yards to meters":
+                outputValue = inputValue / 1.0936
+            case "meters to feet":
+                outputValue = inputValue * 3.2808
+            case "meters to yards":
+                outputValue = inputValue * 1.0936
+            default:
+                print("Show alert for no conversion string")
+            }
+            resultsLabel.text = "\(inputValue) \(fromUnits) = \(outputValue) \(toUnits)"
+        }
+        else{
+            print("Show alert for the value entered is not a number!")
+        }
+        
     }
 
     @IBAction func convertButtonPressed(_ sender: UIButton) {
@@ -45,11 +76,12 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource{
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        conversionString = formulaArray[row]
         let unitsArray = formulaArray[row].components(separatedBy: " to ")
         fromUnits = unitsArray[0]
         toUnits = unitsArray[1]
         fromUnitsLabel.text = fromUnits
-        resultsLabel.text = toUnits
+        calculateConversion()
     }
     
     
